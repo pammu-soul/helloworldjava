1085d6719ca6654a6986153820c6ca6583f435a7
pammu-soul <145240092+pammu-soul@users.noreply.github.com>
2023-09-17 18:53:27 -0400

    Update MyFirstJavaPrj.java

--- MyFirstJavaPrj.java
+++ MyFirstJavaPrj.java
@@ -2,9 +2,15 @@ public class MyFirstJavaPrj {
 	public static void main(String args[]){
 		System.out.println("Wowwww........... This is my MyFirstJavaProject");
 		tempMethod();
+
+		MyFirstJavaPrj ob1 = new MyFirstJavaPrj();
+		obj1.singSong();
 	}
 	public static void tempMethod(){
 		System.out.println(" Hip Hip Hurray! I am being invoked by mainMethod() ");
 	}
-	
-}
+
+	public static void singSong(){
+		System.out.println(" \n\n Sing a Song! O Baby Sing a Song!!! ");
+	}	
+} 
