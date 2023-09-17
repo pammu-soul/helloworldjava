a07ba2f0f9fcac1b962eadc126b371bfbfaecad8
pammu-soul <145240092+pammu-soul@users.noreply.github.com>
2023-09-17 18:45:29 -0400

    Update MyFirstJavaPrj.java

--- MyFirstJavaPrj.java
+++ MyFirstJavaPrj.java
@@ -1,5 +1,10 @@
 public class MyFirstJavaPrj {
 	public static void main(String args[]){
 		System.out.println("Wowwww........... This is my MyFirstJavaProject");
+		tempMethod();
 	}
+	public String tempMethod(){
+		System.out.println(" Hip Hip Hurray! I am being invoked by mainMethod() ")
+	}
+	
 }
